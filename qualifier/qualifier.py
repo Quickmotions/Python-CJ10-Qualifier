13d860ec8b413f61994cacb8bb4a3a7350c638c0
Quickmotions <fergusbhaak@gmail.com>
2023-08-21 13:32:05 +0100

    finished valid_input()

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,27 +1,25 @@
 import cv2
 import numpy
+import math
+
 
 def valid_input(image_size: tuple[int, int], tile_size: tuple[int, int], ordering: list[int]) -> bool:
-    """
-    Return True if the given input allows the rearrangement of the image, False otherwise.
+    if image_size[0] % tile_size[0] != 0 or image_size[1] % tile_size[1] != 0:
+        return False
 
-    The tile size must divide each image dimension without remainders, and `ordering` must use each input tile exactly
-    once.
-    """
+    total_tiles = math.floor(image_size[0]/tile_size[0] * image_size[1]/tile_size[1])
+    if total_tiles != len(ordering):
+        return False
+
+    for tile_num in range(total_tiles):
+        if tile_num not in ordering:
+            return False
     return True
+
 
 def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[int], out_path: str) -> None:
     print(image_path)
-    """
-    Rearrange the image.
 
-    The image is given in `image_path`. Split it into tiles of size `tile_size`, and rearrange them by `ordering`.
-    The new image needs to be saved under `out_path`.
-
-    The tile size must divide each image dimension without remainders, and `ordering` must use each input tile exactly
-    once. If these conditions do not hold, raise a ValueError with the message:
-    "The tile size or ordering are not valid for the given image".
-    """
     image = cv2.imread(image_path)
     height, width, channels = image.shape
     half_height = height // 2
@@ -32,5 +30,4 @@ def rearrange_tiles(image_path: str, tile_size: tuple[int, int], ordering: list[
     cv2.imshow('Top', top_section)
     cv2.imshow('Bottom', bottom_section)
 
-    # cv2.waitKey(0)
-
+    cv2.waitKey(0)
